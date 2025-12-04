881dbd09131090c9b49eab7f28b2acf2ad54f29f
szzd7223 <mohdsaadshaikh17@gmail.com>
2025-12-04 11:24:51 +0530

    improved array conditions code

--- array-conditions.py
+++ array-conditions.py
@@ -3,9 +3,11 @@ def getMaxFromBuiltArray(n):
     arr[0] = 0
     arr[1] = 1
     for i in range(1, (n//2)+1):
-        arr[i*2] = arr[i]
-        arr[(i*2)+1] = arr[i] + arr[i+1]
+        if i*2 < len(arr):       
+            arr[i*2] = arr[i]
+        if (i*2)+1 < len(arr):
+            arr[(i*2)+1] = arr[i] + arr[i+1]
     print(arr)
     print(max(arr))
 
-getMaxFromBuiltArray(7)
+getMaxFromBuiltArray(4)
