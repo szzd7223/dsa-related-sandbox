402e9286defa306085176636351de8d5402c0cfb
szzd7223 <mohdsaadshaikh17@gmail.com>
2025-12-20 23:37:37 +0530

    add solution for leetcode 944

---
+++ Leetcode_Daily_Questions/944_Delete_Columns_To_Make_Sorted.py
@@ -0,0 +1,12 @@
+def minDeletionSize(strs):
+    count = 0
+    rows = len(strs)
+    cols = len(strs[0])
+
+    for col in range(cols):
+        for row in range(rows - 1):
+            if strs[row][col] > strs[row + 1][col]:
+                count += 1
+                break
+
+    return count
