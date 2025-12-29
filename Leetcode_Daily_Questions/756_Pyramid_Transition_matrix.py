37738ed083291174a400bf4b178f0798d446047f
szzd7223 <mohdsaadshaikh17@gmail.com>
2025-12-29 23:54:36 +0530

    leetcode 756 solution

---
+++ Leetcode_Daily_Questions/756_Pyramid_Transition_matrix.py
@@ -0,0 +1,35 @@
+from collections import defaultdict
+import itertools
+
+class Solution:
+    def pyramidTransition(self, bottom: str, allowed: List[str]) -> bool:
+        T = defaultdict(list)
+        for triple in allowed:
+            T[(triple[0], triple[1])].append(triple[2])
+
+        memo = {}
+
+        def solve(row):
+            if len(row) == 1:
+                return True
+            if row in memo:
+                return memo[row]
+
+            options = []
+            for i in range(len(row) - 1):
+                key = (row[i], row[i+1])
+                if key in T:
+                    options.append(T[key])
+                else:
+                    memo[row] = False
+                    return False
+
+            for next_row in itertools.product(*options):
+                if solve("".join(next_row)):
+                    memo[row] = True
+                    return True
+
+            memo[row] = False
+            return False
+
+        return solve(bottom)
